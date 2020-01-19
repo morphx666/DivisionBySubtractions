915071d53ccd618423635796b4c3e9bc5854f098
Xavier Flix <xavier@xfx.net>
2020-01-18 21:55:22 -0400

    Bumped .NET version to 4.8

--- DivisionBySubtractions/My Project/AssemblyInfo.vb
+++ DivisionBySubtractions/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2017.8.21.147")>
-<Assembly: AssemblyFileVersion("2017.8.21.146")>
+<Assembly: AssemblyVersion("2020.1.19.148")>
+<Assembly: AssemblyFileVersion("2020.1.19.147")>
